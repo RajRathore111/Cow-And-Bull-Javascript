c0b8af00114e04b0e9764f596b976b5e69b609cc
Anshul Bahukhandi <revenant.b@gmail.com>
2014-07-14 17:41:16 +0530

    Update CnB.js
    Added Blink on input field when input is wrong. 
    Added fade out & in on click of OK Button
    Added Focus to input field

--- Scripts/CnB.js
+++ Scripts/CnB.js
@@ -1,17 +1,52 @@
+function inputBorderBlink()
+{	
+	$('#userInputField').animate({borderColor: 'red'})
+						.animate({borderColor: '#B3B3B3'})
+						.animate({borderColor: 'red'})
+						.animate({borderColor: '#B3B3B3'})
+						.animate({borderColor: 'red'})
+						.animate({borderColor: '#B3B3B3'}).focus();
+}
+
 $(document).ready(
-function(){
+				function(){
 
-$(".resultScreen").css("display","none");
+					$(".resultScreen").css("display","none");
+					$("#inputBlock").css("display","none");
 
-$("#submitCircle").click(
-						 function(){		
-									if(typeof 	submitCircleVar == "undefined")
-									{					 
-										$("#mainBlock").animate({ "left": "-=350px" }, 1400 );
-										submitCircleVar = "1";						
-										$(".resultScreen").delay(300).slideDown( 1400, function() {$(".resultScreen").css("display","block");});
-									}
-
-								}							
-						);
+					$('#userInputField').bind("enterKey",function(e){
+																	 $("#submitCircle").trigger("click");
+																	});
+					$('#userInputField').keyup(function(e){
+															if(e.keyCode == 13)
+															{
+															$(this).trigger("enterKey");
+															}
+														});
+					$("#submitCircle").click(
+											 function(){														
+														if($('#userInputField').val() === "" || !(/[a-zA-Z]{4}$/.test($('#userInputField').val())))
+														{
+														inputBorderBlink()
+														}
+														else
+														{
+															if($('#userInputField').val().length == 4)
+															{																	
+																if(typeof 	submitCircleVar == "undefined")
+																{					 
+																	$("#mainBlock").animate({ "left": "-=350px" }, 1400 );
+																	submitCircleVar = "1";						
+																	$(".resultScreen").delay(300).slideDown( 1400, function() {$(".resultScreen").css("display","block");});
+																}	
+															}
+														}
+														$('#userInputField').focus();
+													});
+											
+					$(".okButton").click(function(){
+													$("#introBlock").fadeOut("slow",function(){$("#inputBlock").fadeIn();$('#userInputField').focus();});													
+													}
+										);
 });
+
